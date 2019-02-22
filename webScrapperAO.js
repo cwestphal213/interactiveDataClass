f132ba443933ee2961af256a4939462088d4fe4e
cwestphal213 <47224660+cwestphal213@users.noreply.github.com>
2019-02-22 01:06:04 -0500

    Update webScrapperAO.js

--- webScrapperAO.js
+++ webScrapperAO.js
@@ -2,3 +2,12 @@ var s = document.createElement("script")
 s.src = "https://d3js.org/d3.v5.js"
 var body = document.getElementByTagName("body")[0]
 body.after(s)
+
+var linkList = d3.selectAll("div.fusion-text a").nodes()
+var academicList = linkList.map(function(major){
+  return major.innerText
+});
+
+academicList.forEach(function(major){
+  console.log(major)
+})
